ea832d7f26bafb0a35b91daa22fd63d79a28d1b4
lvd01092011 <lethihoa15081995@gmail.com>
2018-11-04 21:22:24 +0700

    Kiem Tra Chan Le

---
+++ Lvd/KiemTraChanLe/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace KiemTraChanLe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap so can kiem tra: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = a%2;
+            if(b==0)
+            {
+                Console.WriteLine("So Chan");
+            }else{
+                Console.WriteLine("Le");
+            }
+        }
+    }
+}
