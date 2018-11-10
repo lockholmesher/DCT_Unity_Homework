6ab5106f41ee10120a851572ce9ca0bcb9ec1e07
Sher Lockholme <lockholme8@gmail.com>
2018-11-10 10:24:10 +0700

    Update Bai Hoc

---
+++ LeTuanAnh/WhileProject/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WhileProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Mảng 1 chiều
+            int[] arrint = new int[5];
+            //Mảng 2 chiều
+            int[,] arrint = new int[5,6];
+            //Mảng của mảng
+            int[][] arr_arrint = new int[5];
+            
+        }
+    }
+}
