2c45b4066d47f8939294ca5fa438de91eeb7c810
Hoangminh11 <leminhchienbka@gmail.com>
2018-11-09 20:58:28 -0800

    Update tu 42-71

---
+++ BT1C#/42.Tinh_tong/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _42.Tinh_tong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,i, tong=0;
+            Console.WriteLine(" So chu chu so cua day:");
+            n=int.Parse(Console.ReadLine());
+            int[] arrNum= new int[n];
+            for (i=0;i<n;i++)
+            {
+             Console.WriteLine(" nhap vao phan tu thu {0}", i);
+             arrNum[i] = Convert.ToInt32(Console.ReadLine());
+             tong = tong + arrNum[i];
+             // Console.WriteLine(" tong cua day {0}", tong);
+            }
+            Console.WriteLine(" tong cua day {0}", tong); 
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BT1C#/43.Hien_thi_hex/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _43.Hien_thi_hex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         int i,j;
+         
+         while ()
+        }
+        string ConvertHex(int Dec)
+        {
+        while(Dec%)  
+        string du;
+        }
+    }
+}
