e2f23237ce8b7dba6a7fc13f8eb9e8bb2346e39b
Hoangminh11 <leminhchienbka@gmail.com>
2018-11-13 07:54:57 -0800

    update 32

---
+++ BT1C#/32.Tam_giac/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _32.Tam_giac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float x,y,z;
+            nhap( out x, out y, out z);
+            ket_qua(x,y,z);
+        }
+        static void nhap(out float x, out float y, out float z)
+        {
+            Console.WriteLine("Nhap canh X:");
+            x=float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap canh Y:");
+            y=float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap canh Z:");
+            z=float.Parse(Console.ReadLine());
+        }
+        static void ket_qua (float x, float y, float z)
+        {
+            if (x+y<=z|| x+z <=y|| z+y<=x|| x<=0||y<=0||z<=0)
+            Console.WriteLine(" Khong phai 3 canh cua tam giac");
+            else
+
+            {
+                if (x==y&&x==z&&z==y)
+                Console.WriteLine("Tam giac deu");
+                else
+                {
+                    if (x==y||x==z||y==x)
+                    Console.WriteLine(" Tam giac can");
+                    else
+                    Console.WriteLine("Tam giac thuong");
+                }
+            }
+           Console.ReadLine();
+        }
+
+    } 
+
+    
+    
+      
+
+}
