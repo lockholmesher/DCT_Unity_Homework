bd8119d57cc6c4dc2c21ab2b59b23570a898be31
Hoangminh11 <leminhchienbka@gmail.com>
2018-11-16 20:15:49 -0800

    update

---
+++ BT1C#/48.Tong_so_le/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _48.Tong_so_le
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         int n;
+         do
+         {
+          Console.WriteLine ("Nhap n =")
+          n=int.Parse(Console.ReadLine());
+         } while (n>=100||n<=0);
+         int [] a = new int [100]
+         for ( int i=0; i<n; i++)
+         {
+             Console.WriteLine( " Nhap a[{0}]", i);
+             a[i]=int.Parse(Console.ReadLine().ToString());
+         }
+         Console.ReadLine();
+         Console.WriteLine("Day so vua nhap:");
+         for(int i=0;i<n;i++)
+         {
+             Console.Write("{0}")
+         }
+        }
+    }
+}
